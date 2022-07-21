c875178e8414c948b702a9ac4aeb458b9cad8c76
GuilhermeKageyama <guiproc@gmail.com>
2022-07-21 16:18:35 -0300

    exemplo redux e next

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import { Provider } from "react-redux";
+import { useStore } from "../redux/store";
+
+function MyApp({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState);
+  return (
+    <>
+      <Head>
+        <link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Provider store={store}>
+        <Navbar />
+        <div className="w-9/12 m-auto pt-10">
+          <Component {...pageProps} />
+        </div>
+      </Provider>
+    </>
+  );
+}
+
+export default MyApp;
